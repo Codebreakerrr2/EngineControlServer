f3dd1eb3c750aea791c43ae924fbf6b303028d9e
usman <wpe118@haw-hamburg.de>
2026-04-22 14:40:03 +0200

    first commit

---
+++ src/main/java/org/example/enginecontrolserver/EngineControlServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.enginecontrolserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineControlServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EngineControlServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/enginecontrolserver/EngineControlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.enginecontrolserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineControlServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
