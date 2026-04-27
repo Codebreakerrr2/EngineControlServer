0b84aadf368907ae220df2c6f45f479fd0757fb6
usman <wpe118@haw-hamburg.de>
2026-04-27 11:07:22 +0200

    added index.html for web gui

---
+++ src/main/java/org/enginecontrolserver/infrastructure/EngineSocketConfig.java
@@ -0,0 +1,13 @@
+package org.enginecontrolserver.infrastructure;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class EngineSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
+}
